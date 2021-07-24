11033e86a887c1c3610d9390367d6f1e6359d707
Lindsay Lopez <lopez95@berkeley.edu>
2021-07-24 13:32:01 -0700

    finished tests/implementation for analyze

--- code/analyze.js
+++ code/analyze.js
@@ -1,9 +1,29 @@
+function average(arr) {
+  return (arr.reduce((sum, current) => {
+    return sum + current;
+  }, 0)) / arr.length;
+}
+
+function min(arr) {
+  return arr.reduce((min, current) => {
+    if (min <= current) return min;
+    return current;
+  });
+}
+
+function max(arr) {
+  return arr.reduce((max, current) => {
+    if (max >= current) return max;
+    return current;
+  });
+}
+
 function analyze(arr) {
   return {
-    average: 4,
-    min: 1,
-    max: 8,
-    length: 6
+    average: average(arr),
+    min: min(arr),
+    max: max(arr),
+    length: arr.length
   };
 }
 
--- test/analyze.test.js
+++ test/analyze.test.js
@@ -7,7 +7,46 @@ const firstArrayAnalyze = {
   length: 6
 };
 
-//test average;
+const secondArray = [4, 6, 2];
+const secondArrayAnalyze = {
+  average: 4,
+  min: 2,
+  max: 6,
+  length: 3
+};
+
+// test average;
 test('testing average of firstArray', () => {
   expect(analyze(firstArray).average).toBe(firstArrayAnalyze.average);
+});
+
+test('testing average of secondArray', () => {
+  expect(analyze(secondArray).average).toBe(secondArrayAnalyze.average);
+});
+
+// test min
+test('testing min of firstArray', () => {
+  expect(analyze(firstArray).min).toBe(firstArrayAnalyze.min);
+});
+
+test('testing min of secondArray', () => {
+  expect(analyze(secondArray).min).toBe(secondArrayAnalyze.min);
+});
+
+// test max
+test('testing max of firstArray', () => {
+  expect(analyze(firstArray).max).toBe(firstArrayAnalyze.max);
+});
+
+test('testing max of secondArray', () => {
+  expect(analyze(secondArray).max).toBe(secondArrayAnalyze.max);
+});
+
+// test length
+test('testing length of firstArray', () => {
+  expect(analyze(firstArray).length).toBe(firstArrayAnalyze.length);
+});
+
+test('testing length of secondArray', () => {
+  expect(analyze(secondArray).length).toBe(secondArrayAnalyze.length);
 });
